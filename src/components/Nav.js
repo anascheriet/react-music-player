a38c72ca268cebd8c61c76896695df08103fed4b
Anas CHERIET <anascheriet@hotmail.com>
2020-11-26 20:02:30 +0100

    "Github Link"

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,17 +1,20 @@
 import React from 'react'
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMusic } from "@fortawesome/free-solid-svg-icons"
+import { faMusic, faGithub } from "@fortawesome/free-solid-svg-icons"
 
 export const Nav = ({ libraryStatus, setLibraryStatus }) => {
     return (
         <nav className="nav">
-            <h1>Waves <p style={{fontSize: '0.5rem'}}>by anasCH</p></h1>
-            <button className={libraryStatus ? "library-active" : ""} onClick={() => setLibraryStatus(!libraryStatus)}>
+            <h1>Waves <p style={{ fontSize: '0.5rem' }}>by anasCH</p></h1>
+            <button className={libraryStatus ? "library-active" : ""}
+                onClick={() => setLibraryStatus(!libraryStatus)}>
                 Library
                 <FontAwesomeIcon icon={faMusic} />
             </button>
-            <button >Github</button>
+            <button ><a style={{ textDecoration: "none", color: "black" }} href="http://github.com/anascheriet" target="blank">Github</a>
+                <i class="fab fa-github"></i>
+            </button>
         </nav>
     )
 }
